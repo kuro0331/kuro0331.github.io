0908b21b46093745aaa17942d0148d3e20851c59
kuro0331 <kuro0331@gmail.com>
2024-07-24 02:59:18 +0900

    1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "465138034"
+    serviceWorkerVersion: "4196096414"
   }
 });
